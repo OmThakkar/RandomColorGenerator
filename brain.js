afc988d215b028962e9a2929cec3bf00dd6beb1b
OmThakkar <omthakkar2021.comp@mmcoe.edu.in>
2022-09-11 20:09:46 +0530

    completed using for loop

--- brain.js
+++ brain.js
@@ -1,4 +1,4 @@
-const bgcolor = () =>{
+/*const bgcolor = () =>{
     const randomnumber = Math.floor(Math.random() * 16777215);
     const randomcode = "#" +  randomnumber.toString(16);
     document.body.style.backgroundColor = randomcode;
@@ -7,4 +7,21 @@ const bgcolor = () =>{
 
 document.getElementById("btn").addEventListener("click", bgcolor)
 
-bgcolor(); 
+bgcolor(); */
+
+function bgcolor(){
+    var random = "ABCDEF0123456789";
+    var randomcode = "#";
+
+    for(var i = 0; i <6 ; i++) {
+        randomcode += random[Math.floor(Math.random()*16)];
+    }
+
+    document.body.style.backgroundColor = randomcode;
+    document.getElementById("colorcode").innerText = randomcode;
+
+}
+
+
+
+bgcolor();
